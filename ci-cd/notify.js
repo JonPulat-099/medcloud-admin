41ea5369ab0eb74d8d53939da93c3abce3e1f68e
JonPulat-099 <rjpm023@gmail.com>
2023-03-17 07:09:27 +0500

    Change actions

--- ci-cd/notify.js
+++ ci-cd/notify.js
@@ -1,21 +1,17 @@
-const axios = require('axios');
+const axios = require('axios')
 
-module.exports = async ({context, telegram}) => {
-  
-  const url = 'https://api.telegram.org/bot';
+module.exports = async ({ context, telegram }) => {
+  const url = 'https://api.telegram.org/bot'
 
-  let msg = `📣 Medcloud Alert`
-    + `${JSON.stringify(context)}`
-    + ` развёрнут на [BETA](https://beta.ipakyulibank.uz)`;
+  let msg =
+    `📣 Medcloud Alert` +
+    `${JSON.stringify(context)}` +
+    ` развёрнут на [BETA](https://beta.ipakyulibank.uz)`
 
-
-  await axios.post(
-    `${url}${telegram.token}/sendMessage`,
-    {
-      chat_id: telegram.chat_id,
-      parse_mode: 'MarkdownV2',
-      text: msg
-    }
-  );
-  return true;
+  await axios.post(`${url}${telegram.token}/sendMessage`, {
+    chat_id: telegram.chat_id,
+    parse_mode: 'MarkdownV2',
+    text: msg,
+  })
+  return true
 }
